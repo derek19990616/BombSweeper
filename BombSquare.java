ee0e1fe6b6a9387cd8c769b8af510718ec951496
derek19990616@gmail.com <derek19990616@github.com>
2020-10-23 16:02:45 +0800

    adding basic clicking fuction

--- BombSquare.java
+++ BombSquare.java
@@ -2,14 +2,150 @@ public class BombSquare extends GameSquare
 {
     private GameBoard board;                            // Object reference to the GameBoard this square is part of.
     private boolean hasBomb;                            // True if this squre contains a bomb. False otherwise.
+    private int widthForBoard;
+    private int heightForBoard;
 
+    
+    
+    private boolean buttonClicked;
+    private boolean buttonFlagged;
+    private int countNum = 0;
+    private boolean gameFail = false;
+    
+ 
+  
+    
 	public static final int MINE_PROBABILITY = 10;
 
 	public BombSquare(int x, int y, GameBoard board)
 	{
-		super(x, y, "images/blank.png");
+        super(x, y, "images/blank.png");
+        this.widthForBoard = x;
+        this.heightForBoard = y;
 
         this.board = board;
+        
         this.hasBomb = ((int) (Math.random() * MINE_PROBABILITY)) == 0;
-    }    
+
+        
+        
+       
+    }
+    public boolean getBombStatus(){
+        return this.hasBomb;
+        
+    }
+    public boolean getFailStatus(){
+        return this.gameFail;
+    }
+
+
+
+    public boolean getButtonClickStatus(){
+        return this.buttonClicked;
+    }
+     public void setButtonClickStatus(){
+        this.buttonClicked = false;
+    }
+    public void setCountNum(int countNum){
+        this.countNum = countNum;
+
+    
+    }
+    public int getCountNum(){
+        return this.countNum;
+    }
+
+    public void setImageBasedOnNum(int i, int x, int y){
+        if(getButtonClickStatus() == false){
+            switch(i){
+                case 0:
+                board.getSquareAt(x, y).setImage("images/0.png");
+                break;
+                case 1:
+                board.getSquareAt(x, y).setImage("images/1.png");
+                break;
+                case 2:
+                board.getSquareAt(x, y).setImage("images/2.png");
+                break;
+                case 3:
+                board.getSquareAt(x, y).setImage("images/3.png");
+                break;
+                case 4:
+                board.getSquareAt(x, y).setImage("images/4.png");
+                break;
+                case 5:
+                board.getSquareAt(x, y).setImage("images/5.png");
+                break;
+                case 6:
+                board.getSquareAt(x, y).setImage("images/6.png");
+                break;
+                case 7:
+                board.getSquareAt(x, y).setImage("images/7.png");
+                break;
+                case 8:
+                board.getSquareAt(x, y).setImage("images/8.png");
+                break;
+            }
+        }
+    }
+
+   
+    
+   // @overide
+   /**
+    * This method allows user to click the selected grid on board.
+    */
+    public void leftClicked(){
+       
+        int x = super.getXLocation();
+        int y = super.getYLocation();
+        if(buttonFlagged == false){
+            if(buttonClicked == false){
+                if(getBombStatus()){
+                    board.getSquareAt(x, y).setImage("images/bomb.png");
+                    System.out.println("Position " + x + " " + y + " with bomb been clicked");
+                    this.gameFail = true;
+                }
+                else{
+                    setImageBasedOnNum(getCountNum(),x, y);
+                    System.out.println("Position " + x + " " + y + " with number " + getCountNum() + " been clicked");
+
+                }
+               
+            }
+            this.buttonClicked = true;
+            
+
+        }
+        
+    }
+     // @overide
+     /**
+      * This right clicked function ensure that once the cell has been flagged, it will not be able to leftclicked.
+      */
+    public void rightClicked(){
+        int x = super.getXLocation();
+        int y = super.getYLocation();
+        if(buttonClicked == false){
+            if(buttonFlagged == false){
+                board.getSquareAt(x,y).setImage("images/flag.png");
+                buttonFlagged = true;
+            }
+            else{
+                board.getSquareAt(x,y).setImage("images/blank.png");
+                buttonFlagged = false;
+            }
+        }
+    }
+
+ 
 }
+
+    
+
+
+    
+    
+ 
+
--- Driver.java
+++ Driver.java
@@ -1,7 +1,67 @@
+import java.util.*;
 public class Driver
 {
+
+	
 	public static void main(String[] Args)
-	{
-		GameBoard b = new GameBoard("BugSweeper", 30, 30, "BombSquare");
+	{   
+		int x;
+		int y;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the size of your game for x axis");
+		x = (int)input.nextInt();
+		System.out.println("Please enter the size of your game for y axis");
+		y = (int)input.nextInt();
+
+		GameBoard b = new GameBoard("BugSweeper", x, y, "BombSquare");
+		BombSquare[][] bs = new BombSquare[x][y];
+		boolean[][] boardMine = new boolean[x][y];
+		for(int i = 0 ; i < x; i++){
+			for(int j = 0; j < y; j++){
+				bs[i][j] = new BombSquare(i, j, b);
+				if(bs[i][j].getBombStatus())
+				boardMine[i][j] = true;
+				else boardMine[i][j] = false;
+	
+			}
+		}
+		for(int i = 0 ; i < x; i++){
+			for(int j = 0; j < y; j++){
+                if(boardMine[i][j] != true){
+				    bs[i][j].setCountNum(checkNumSurroundBomb(i, j, boardMine, x, y));
+					System.out.println("Position "+ i + " " + j + " " + " with num " + bs[i][j].getCountNum());
+				}
+				else if(boardMine[i][j] == true){
+					System.out.println("Position "+ i + " " + j + " " + " with bomb ");
+
+				}
+			}
+		}
+	   
+		
+
+
+		
+
 	}
+	 private static int checkNumSurroundBomb(int i, int j, boolean[][] boardMine, int x, int y){
+       
+        int countMine = 0;    
+        if(boardMine[i][j] == false){
+            
+            if(i!=0 && j!=0 && boardMine[i-1][j-1] == true) countMine++;           //top left
+            if(i!=0 && boardMine[i-1][j] == true) countMine++;                     //top
+            if(i!=0 && j < y-1 && boardMine[i-1][j+1] == true) countMine++;          //top right
+            if(j!=0 && boardMine[i][j-1] == true) countMine++;                     //left
+            if(j < x-1 && boardMine[i][j+1] == true) countMine++;                    //right
+            if(i < x-1 && j!=0 && boardMine[i+1][j-1] == true) countMine++;          //down left
+            if(i < x-1 && boardMine[i+1][j] == true) countMine++;                    //down
+            if(i < x-1 && j < y-1 && boardMine[i+1][j+1] == true) countMine++;         //down right
+            
+        }
+        return countMine;
+
+	}
+	
+	
 }
